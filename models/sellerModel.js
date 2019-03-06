7c49062cde6a6c13e0396f579693b41cb81ac9d8
Jhonatan Cruz <jcruz3@drew.edu>
2019-03-06 03:01:06 -0500

    added seller database Model

---
+++ models/sellerModel.js
@@ -0,0 +1,22 @@
+let mongoose = require('mongoose');
+
+
+var Schema = mongoose.Schema;
+
+var itemDataSchema= new Schema({
+  title: {type: String, required:true},
+  description: String,
+  price: Number,
+  imgs: Array,
+  datePosted: Number,
+  datePostedComputed: String,
+  dateSold: Date,
+  postedBy: String,
+  boughtBy: String,
+  transactionDate: Date,
+  priceSoldAt: Number
+},{collection:'listings'});
+
+var itemData = mongoose.model('itemData', itemDataSchema);
+
+module.exports = itemData;
