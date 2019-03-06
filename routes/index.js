7dff38a29771ecd3a68d790857e47734e2f2f37f
Jhonatan Cruz <jcruz3@drew.edu>
2019-03-06 03:07:39 -0500

    imported sellers model, retrived all listings in a descending order from the time it was posted

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var mongoose= require('mongoose');
+var itemData = require('../models/sellerModel');
 mongoose.connect('mongodb://heroku_v3r3b96l:rdihvrpq58acjbaole0f7jbo7c@ds127802.mlab.com:27802/heroku_v3r3b96l');
 
-// get request all listings 
-
+console.log(itemData);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DrewUse'});
+  itemData.find().sort( { datePosted: -1 } )
+    .then(function(doc) {
+      res.render('index', { title: 'DrewUse', items:doc});
+    });
 });
+
 
 
 
