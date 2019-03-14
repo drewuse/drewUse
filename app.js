0d69491f7dc394f6253760ff9bb4e774fa734601
jhonatancruz <jcruz3@drew.edu>
2019-03-14 01:36:24 -0400

    attempting to get google oauth to work on site

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var sellRouter = require('./routes/sell');
 var profileRouter = require('./routes/profile')
-var testRouter = require('./routes/test')
+var googleAuthRouter = require('./routes/authenticating')
 
 var hbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 var app = express();
 
@@ -31,7 +33,7 @@ app.use(expressSession({secret:'max', saveUninitialized:false, resave:false}))
 app.use('/', indexRouter);
 app.use('/sell', sellRouter);
 app.use('/profile', profileRouter);
-app.use('/test', testRouter)
+app.use('/authenticating', googleAuthRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,5 +50,20 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Set up passport strategy
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
+    callbackURL: 'https://drewuse.herokuapp.com/auth/google/callback',
+    scope: ['email'],
+  },
+  // This is a "verify" function required by all Passport strategies
+  (accessToken, refreshToken, profile, cb) => {
+    console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
+    return cb(null, profile);
+  },
+));
 
 module.exports = app;
